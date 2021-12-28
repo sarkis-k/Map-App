165f5b6bbb0483a1331187b42efb37e60d702204
sarkis-k <sargis.leva@gmail.com>
2021-12-28 11:05:37 -0800

    advanced popup bar for markers

--- map1.py
+++ map1.py
@@ -7,19 +7,27 @@ lon=list(data["LON"])
 mt_name=list(data["NAME"])
 elev=list(data["ELEV"])
 
-html = """ 
-<h3>Volcano Information:</h3>
+#basic html
+# html = """
+# <h3>Volcano Information:</h3>
+# Height: %s m
+# """
+
+#advanced html
+html="""
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
 Height: %s m
 """
 
-map = folium.Map(location=[34.1442783,-118.2535569], zoom_start=10, tiles="Stamen Terrain")
+map = folium.Map(location=[34.1442783,-118.2535569], zoom_start=5, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="my map")
 
 
 
 for coorX, coorY, nm, el in zip(lat,lon,mt_name, elev):
-	iframe = folium.IFrame(html=html %str(el), width=200, height=100)
+	iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
 	fg.add_child(folium.Marker(location=[coorX,coorY], popup=folium.Popup(iframe), icon=folium.Icon(color='red')))
 	
 map.add_child(fg)
