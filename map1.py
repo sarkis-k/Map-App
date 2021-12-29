a4734a91316b53760d0110619d59c9b9e9b5c2ec
sarkis-k <sargis.leva@gmail.com>
2021-12-29 12:06:29 -0800

    added population layer, separated layers and added layer control feature

--- map1.py
+++ map1.py
@@ -33,7 +33,7 @@ elev=list(data["ELEV"])
 
 map = folium.Map(location=[34.1442783,-118.2535569], zoom_start=5, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="my map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 
 #for basic and advanced html popup
@@ -43,9 +43,22 @@ fg = folium.FeatureGroup(name="my map")
 #icon=folium.Icon(color=(lambda el: "red" if el>2000 else "green")
 
 for coorX, coorY, nm, el in zip(lat,lon,mt_name, elev):
-	fg.add_child(folium.Marker(location=[coorX,coorY], popup=str(el)+" m",  icon=folium.Icon(color=color_producer(el)) ))
+	fgv.add_child(folium.CircleMarker(
+		location=[coorX,coorY], 
+		radius=6,
+		popup=str(el)+" m",
+		fill_color=color_producer(el),
+		color="gray",
+		fill_opacity=0.7 ))
 
-	
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
 
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+	style_function=lambda x: {"fillColor":"yellow" if x["properties"]["POP2005"]<10000000 else "green" if 10000000 <=x["properties"]["POP2005"]<100000000 else "blue"}))
+
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
